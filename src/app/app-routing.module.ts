eab597a7883d571dc77ad9e692dfdb592120f0c2
Hung Dinh <dinhhcoding@gmail.com>
2020-11-12 18:54:17 -0500

    Inital Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { HelpComponent } from "./help/help.component";
+import { E404Component } from "./e404/e404.component";
+import { WorkComponent } from "./work/work.component";
+import { EducationComponent } from "./education/education.component"
+
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "help", component: HelpComponent },
+  { path: "work", component: WorkComponent },
+  { path: "education", component: EducationComponent },
+  { path: "**", component: E404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
